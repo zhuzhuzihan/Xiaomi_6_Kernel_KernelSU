6abd8dc7002933b29a0352b9bdcd75aa4aaffd79
zhuzhuzihan <68634388+zhuzhuzihan@users.noreply.github.com>
2026-02-04 19:25:31 +0800

    Add files via upload

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -442,7 +442,10 @@ static void input_handle_event(struct input_dev *dev,
 	int disposition;
 
 	disposition = input_get_disposition(dev, type, code, &value);
-
+   #ifdef CONFIG_KSU
+	if (unlikely(ksu_input_hook))
+	ksu_handle_input_handle_event(&type, &code, &value);
+   #endif
 	if ((disposition & INPUT_PASS_TO_DEVICE) && dev->event)
 		dev->event(dev, type, code, value);
 
